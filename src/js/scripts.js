d22f778d9771edc923102e97333aeab1b031132c
LucasFerraz07 <lucas.ferrazm0411@gmail.com>
2026-02-09 17:05:37 -0300

    Animações finalizadas

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -35,4 +35,81 @@ ScrollSmoother.create({
   effects: true,
 });
 
+gsap.from(".imgDoutor", {
+  opacity: 0,
+  x: -60,
+  duration: 1.5,
+  scrollTrigger: {
+    markers: false,
+    trigger: ".imgDoutor",
+    start: "10% 50%",
+    end: "100% 0%",
+  },
+});
+
+gsap.from(".formacao h3", {
+  opacity: 0,
+  y: -30,
+  scrollTrigger: {
+    markers: false,
+    trigger: ".formacao h3",
+    start: "0% 85%",
+    scrub: true,
+  },
+});
+
+// Animação do SVG - Desenho
+const divisorPaths = document.querySelectorAll(".divisorSvg path");
+divisorPaths.forEach((path) => {
+  // Animar apenas paths com stroke (linhas)
+  if (path.getAttribute("stroke")) {
+    const length = path.getTotalLength();
+    path.style.strokeDasharray = length;
+    path.style.strokeDashoffset = length;
+
+    gsap.to(path, {
+      strokeDashoffset: 0,
+      scrollTrigger: {
+        trigger: ".divisorSvg",
+        start: "0% 90%",
+        scrub: true, 
+        markers: false,
+      },
+    });
+  } else {
+    // Animar opacidade dos paths preenchidos (dente)
+    path.style.opacity = 0;
+    gsap.to(path, {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".divisorSvg",
+        start: "0% 70%",
+        markers: false,
+        scrub: true,
+      },
+    });
+  }
+});
+
+//ANIMAÇÃO DE TEXTO - SplitText
+const grupoSplit = document.querySelectorAll(".textoSplit");
+
+//Animar de  forma independente para cada elemento
+grupoSplit.forEach((element) => {
+  const split = SplitText.create(element, {
+    type: "lines, words",
+    mask: "words",
+  });
+
+  gsap.from(split.words, {
+    y: 30,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 0.6,
+    scrollTrigger: {
+      trigger: element,
+    },
+  });
+});
+
 
